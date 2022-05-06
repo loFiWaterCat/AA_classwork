79b1fd9c40295cbcb1d8ab24113c8b33138f7f0c
ablin2012 <adamlin.2009@gmail.com>
2022-05-06 12:11:03 -0700

    first driver switch

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,15 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
+    
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
 
+    def parent=(new_node)
+        @parent.children.select!
+        @parent = new_node
+        @parent.children << self if new_node != nil
+    end
 end
