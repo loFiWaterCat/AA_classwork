74c2fbe6c3e88db4eca97253d53284cf8889ea95
alan <alan001tk@gmail.com>
2022-05-06 16:14:45 -0700

    Finished valid_moves for knight_travails

--- W3D5/knights_travails/knight.rb
+++ W3D5/knights_travails/knight.rb
@@ -10,8 +10,22 @@ class KnightPathFinder
   end
 
   def self.valid_moves(pos)
-    
+    variations = [[1, 2], [2, 1], [1, -2], [-2, 1],
+                  [2, -1], [-1, 2], [-1, -2], [-2, -1]]
+
+    valid_pos = []
+
+    variations.each do |move|
+      row = pos[0] + move[0]
+      col = pos[1] + move[1]
+      if row.between?(0, 7) &&
+          col.between?(0, 7)
+           valid_pos << [row, col]
+      end
+    end
+    valid_pos
   end
+
   def new_move_positions
   end
 end
