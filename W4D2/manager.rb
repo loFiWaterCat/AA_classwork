46d41955589bafd554230099874bdf0421adbf3d
alan <alan001tk@gmail.com>
2022-05-10 13:39:37 -0700

    Finished Employee and Manager

--- W4D2/manager.rb
+++ W4D2/manager.rb
@@ -1,3 +1,5 @@
+require_relative "employee.rb"
+
 class Manager < Employee
     def initialize(name, title, salary, boss)
         super
@@ -7,8 +9,16 @@ class Manager < Employee
     def bonus(multiplier)
         total_salary = 0
         @employees.each do |emp|
-            total_salary += emp.salary
+            total_salary += emp.bonus(multiplier)
+            if emp.is_a?(Manager)
+              total_salary += emp.salary * multiplier
+            end
         end
-        
+
+        total_salary
+    end
+
+    def add_employee(employee)
+      @employees << employee
     end
 end
