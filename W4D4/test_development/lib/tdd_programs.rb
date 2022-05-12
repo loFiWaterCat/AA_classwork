0e8fcb2cae283d84e4ea10e4e0dc225c783df98c
alan <alan001tk@gmail.com>
2022-05-12 14:03:46 -0700

    Lucy takes the wheel

--- W4D4/test_development/lib/tdd_programs.rb
+++ W4D4/test_development/lib/tdd_programs.rb
@@ -19,4 +19,32 @@ class Array
         end
         pairs
     end
+
+    def my_transpose
+      transpose = Array.new(self.length) {Array.new(self.length)}
+      (0...self.length).each do |i|
+        (0...self.length).each do |j|
+           transpose[i][j] = self[j][i]
+        end
+      end
+      transpose
+    end
+
+    def stock_picker
+      best_i = 0
+      best_j = 0
+      profit = 0
+      (0...self.length - 1).each do |i|
+        (i + 1...self.length).each do |j|
+          new_profit = self[j] - self[i]
+          if new_profit > profit
+            profit = new_profit
+            best_i = i
+            best_j = j
+          end
+        end
+      end
+
+      [best_i, best_j]
+    end
 end
--- W4D4/test_development/spec/tdd_programs_spec.rb
+++ W4D4/test_development/spec/tdd_programs_spec.rb
@@ -3,7 +3,7 @@ require "tdd_programs.rb"
 
 
 describe "#my_uniq" do
-    it "removes duplicates from an array" do 
+    it "removes duplicates from an array" do
         expect([1,2,1,3,3].my_uniq).to eq([1,2,3])
     end
 end
@@ -12,4 +12,21 @@ describe "#two_sum" do
     it "returns an array of the indices of pairs that sum to 0" do
         expect([-1,0,2,-2,1].two_sum).to eq([[0,4],[2,3]])
     end
+end
+
+describe "#my_transpose" do
+  it "returns a 2d array that is the the transpose of the original 2d array" do
+    expect([
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]].my_transpose).to eq([[0, 3, 6],
+                       [1, 4, 7],
+                       [2, 5, 8]])
+  end
+end
+
+describe "#stock_picker" do
+  it "returns a pair of the days where buying and selling would be the most profitable" do
+    expect([10, 1, 5, 4, 10].stock_picker).to eq([1, 4])
+  end
 end
