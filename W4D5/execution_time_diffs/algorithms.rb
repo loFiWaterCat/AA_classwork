53c62af6202145380cb64f2dd76de2cdae2ed63a
alan <alan001tk@gmail.com>
2022-05-13 13:59:16 -0700

    Finished phase 1 of largest_cont_sum

--- W4D5/execution_time_diffs/algorithms.rb
+++ W4D5/execution_time_diffs/algorithms.rb
@@ -1,3 +1,4 @@
+# O(n^2) - quadratic
 def my_min(list)
     list.each_with_index do |num, i|
         list.each_with_index do |num_2, j|
@@ -18,10 +19,32 @@ list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # puts my_min(list)
 
 
+# O(n) - linear
 def my_min_2(list)
     min = list[0]
     list.each { |num| min = num if num < min }
     min
 end
 
-puts my_min_2(list)
+# puts my_min_2(list)
+
+
+# O(n^2) - quadratic
+def largest_cont_sum(list)
+  arr = []
+
+  list.each_with_index do |num1, i|
+    list.each_with_index do |num2, j|
+      if j >= i
+        arr << list[i..j].sum
+      end
+    end
+  end
+
+  arr.max
+end
+
+list2 = [2, 3, -6, 7, -6, 7]
+puts largest_cont_sum(list2)
+
+
