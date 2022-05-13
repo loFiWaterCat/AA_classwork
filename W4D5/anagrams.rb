8d557395c29fa4ef24f4101b05793c2887dae798
Quang Tran <quang.tran.769869@gmail.com>
2022-05-13 16:25:16 -0700

    finished anagrams

--- W4D5/anagrams.rb
+++ W4D5/anagrams.rb
@@ -1,4 +1,4 @@
-def anagram?(str1, str2)
+def anagram?(str1, str2) #O(n!) - factorial
   perms = str1.split("").permutation.to_a.map {|word| word.join}
   perms.include?(str2)
 end
@@ -6,7 +6,7 @@ end
 # puts anagram?("gizmo", "sally")    #=> false
 # puts anagram?("elvis", "lives")    #=> true
 
-def second_anagram?(str1, str2)
+def second_anagram?(str1, str2) #O(n^3) polynomial
   str1_chars = str1.split("")
   str2_chars = str2.split("")
 
@@ -22,5 +22,25 @@ def second_anagram?(str1, str2)
   true
 end
 
-puts second_anagram?("gizmo", "sally")    #=> false
-puts second_anagram?("elvis", "lives")    #=> true
+# puts second_anagram?("gizmo", "sally")    #=> false
+# puts second_anagram?("elvis", "lives")    #=> true
+
+def third_anagram?(str1, str2) # O(n log n)
+  str1.split("").sort == str2.split("").sort
+end
+
+# puts third_anagram?("gizmo", "sally")    #=> false
+# puts third_anagram?("elvis", "lives")    #=> true
+
+def fourth_anagram?(str1, str2)
+  hash1 = Hash.new(0)
+  hash2 = Hash.new(0)
+  str1.each_char { |c| hash1[c] += 1 }
+  str2.each_char { |c| hash2[c] += 1 }
+  hash1 == hash2
+end
+
+puts fourth_anagram?("gizmo", "sally")    #=> false
+puts fourth_anagram?("elvis", "lives")    #=> true
+puts fourth_anagram?("zzzxxy", "yxxzzz")  #=> true
+puts fourth_anagram?("zzzxxy", "yxxzz")   #=> false
