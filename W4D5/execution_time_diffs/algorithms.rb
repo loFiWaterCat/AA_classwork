a3c04d91b0aa073b856322a632e0cca5a68125b0
Quang Tran <quang.tran.769869@gmail.com>
2022-05-13 13:39:34 -0700

    finished my min

--- W4D5/execution_time_diffs/algorithms.rb
+++ W4D5/execution_time_diffs/algorithms.rb
@@ -15,4 +15,13 @@ def my_min(list)
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-puts my_min(list)
+# puts my_min(list)
+
+
+def my_min_2(list)
+    min = list[0]
+    list.each { |num| min = num if num < min }
+    min
+end
+
+puts my_min_2(list)
