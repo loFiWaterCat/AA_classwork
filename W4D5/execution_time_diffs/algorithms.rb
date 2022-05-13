cec4326acae1178aadc9e27e4c3c296cef8dcd61
Quang Tran <quang.tran.769869@gmail.com>
2022-05-13 15:34:39 -0700

    finished execution time algos

--- W4D5/execution_time_diffs/algorithms.rb
+++ W4D5/execution_time_diffs/algorithms.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 # O(n^2) - quadratic
 def my_min(list)
     list.each_with_index do |num, i|
@@ -29,22 +31,56 @@ end
 # puts my_min_2(list)
 
 
-# O(n^2) - quadratic
+# O(n^3) - polynomial
 def largest_cont_sum(list)
   arr = []
 
   list.each_with_index do |num1, i|
     list.each_with_index do |num2, j|
       if j >= i
-        arr << list[i..j].sum
+        arr << list[i..j]
       end
     end
   end
-
-  arr.max
+  arr.map { |sub| sub.sum }.max
+  
 end
 
 list2 = [2, 3, -6, 7, -6, 7]
 puts largest_cont_sum(list2)
 
+def largest_cont_sum_2(list)
+    cur = list[0]
+    lrg = list[0]
+    i = 0
+    j = 0
+    reset = false
+    # debugger
+    (1..list.length - 1).each do |idx| #starts loop at second element
+        cur += list[idx] #current += next element
+        if cur > lrg 
+            if reset
+                i = idx
+            end
+            lrg = cur
+            j = idx  
+            reset = false
+        elsif list[idx] > lrg
+            lrg = list[idx]
+            i = idx
+            j = idx
+            cur = lrg
+        end
+        if (idx + 1 < list.length - 1) && cur + list[idx + 1] <= list[idx + 1]
+            cur = list[idx + 1]
+            reset = true
+        end
+    end
+    list[i..j].sum
+    # p list[i..j]
+end
+
+list3 = [-5, -1, -3]
+
+puts largest_cont_sum_2(list2)
 
