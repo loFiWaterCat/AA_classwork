34ea0178e4da1719fdc372494048b16960309148
alan <alan001tk@gmail.com>
2022-05-16 16:32:34 -0700

    Did some work on phase 4, linked list

--- W5D1/skeleton/lib/p04_linked_list.rb
+++ W5D1/skeleton/lib/p04_linked_list.rb
@@ -20,7 +20,12 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @tail.prev = @head
+    @head.next = @tail
   end
 
   def [](i)
@@ -35,21 +40,57 @@ class LinkedList
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    next_node = @head
+    while next_node != nil
+      return next_node.val if next_node.key == key
+      next_node = next_node.next
+    end
   end
 
   def include?(key)
+    next_node = @head
+    while next_node != nil
+      return true if key == next_node.key
+      next_node = next_node.next
+    end
+    false
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+    old_node = @tail.prev
+    old_node.next = node
+    node.prev = old_node
+    @tail.prev = node
+    node.next = @tail
   end
 
   def update(key, val)
+    next_node = @head
+    while next_node != nil
+      next_node.val = val if next_node.key == key
+      next_node = next_node.next
+    end
   end
 
   def remove(key)
+    next_node = @head
+    while next_node != nil
+      if next_node.key == key
+        current = next_node.prev
+        next_next_node = next_node.next
+
+        current.next = next_next_node
+        next_next_node.prev = current
+        break
+      end
+
+      next_node = next_node.next
+    end
   end
 
   def each
