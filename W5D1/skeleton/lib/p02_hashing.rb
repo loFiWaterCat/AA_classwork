c1fca8753b4867889cd7d7249c9935ccba264cf4
alan <alan001tk@gmail.com>
2022-05-16 15:06:33 -0700

    Finished phase 2

--- W5D1/skeleton/lib/p02_hashing.rb
+++ W5D1/skeleton/lib/p02_hashing.rb
@@ -4,11 +4,24 @@ end
 
 class Array
   def hash
+    total_hash = 0
+    self.each_with_index do |el, ind|
+      total_hash += (el * ind) * 10
+    end
+
+    total_hash
+
   end
 end
 
 class String
   def hash
+    total_hash = 0
+    self.each_char.with_index do |c, ind|
+      total_hash += (c.ord * ind) * 9
+    end
+
+    total_hash
   end
 end
 
@@ -16,6 +29,11 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    total_hash = 0
+
+    self.each do |k, v|
+      total_hash += k.to_s.ord + v.to_s.ord * 10
+    end
+    total_hash
   end
 end
