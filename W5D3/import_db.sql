3b0d95608bb31601af7ebe8169b1d387f607a3ab
Zhile Lin <zhlin04@gmail.com>
2022-05-18 11:38:41 -0700

    add INSERT INTO

--- W5D3/import_db.sql
+++ W5D3/import_db.sql
@@ -1,10 +1,18 @@
-CREATE TABLE users(
+PRAGMA foreign_keys = ON;
+
+DROP TABLE if exists question_likes;
+DROP TABLE if exists replies;
+DROP TABLE if exists question_follows;
+DROP TABLE if exists questions;
+DROP TABLE if exists users;
+
+CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
-    lname TEXT NOT NULL,
+    lname TEXT NOT NULL
 );
 
-CREATE TABLE questions(
+CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
@@ -15,16 +23,16 @@ CREATE TABLE questions(
 );
 
 
-CREATE question_follows (
+CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     question_id INTEGER NOT NULL,
 
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
-)
+);
 
-CREATE replies (
+CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
     question_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
@@ -34,4 +42,25 @@ CREATE replies (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
-)
+);
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+
+);
+
+INSERT INTO
+     users(fname,lname)
+VALUES
+  ('ALAN','TRAN'),
+  ('Brian',"Lin");
+
+INSERT INTO
+   questions(title,body,user_id)
+ VALUES
+    ("Weight","HOW MUCH DO YOU LIFT??!",(SELECT id FROM users WHERE fname = "Brian" AND lname = "Lin"));
