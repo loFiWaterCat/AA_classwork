f1c0e8e19e533bf626aeb96d9f0a6776e7f2f610
Zhile Lin <zhlin04@gmail.com>
2022-05-18 14:38:43 -0700

    finish USER CLASS, start on Question

--- W5D3/QuestionsDatabase.rb
+++ W5D3/QuestionsDatabase.rb
@@ -23,10 +23,63 @@ class User
     @id = options['id']
     @fname = options['fname']
     @lname = options['lname']
+
+  end
+
+  def self.find_by_id(id)
+
+      
+
+       user = QuestionsDatabase.instance.execute(<<-SQL, id)
+
+       SELECT
+         *
+       FROM
+        users
+       WHERE
+        id = ?
+     SQL
+
+    #  raise "#{self} not in database" unles
+     p "self is #{self} "
+     p user
+    #  User.new(user.first)
+  end
+
+
+  def self.find_by_name(fname)
+
+    user = QuestionsDatabase.instance.execute(<<-SQL, fname)
+
+       SELECT
+         *
+       FROM
+        users
+       WHERE
+        fname = ?
+     SQL
+     
+     User.new(user[0])
+
   end
 end
 
 class Question
+  attr_accessor :id, :title , :body , :user_id
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
+    data.map { |datum| Question.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body= options['body']
+    @user_id = options['user_id']
+
+  end
+
 end
 
 class QuestionFollow
