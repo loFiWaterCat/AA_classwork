eee8d3183cf6eeeecb50aa28a9bb6ce94c9a71dd
alan <alan001tk@gmail.com>
2022-05-18 10:52:05 -0700

    Added a few more tables to import_db.sql. Still need to finish it.

--- W5D3/import_db.sql
+++ W5D3/import_db.sql
@@ -15,4 +15,23 @@ CREATE TABLE questions(
 );
 
 
-CREATE question_follows ()
+CREATE question_follows (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+)
+
+CREATE replies (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    body TEXT NOT NULL,
+    parent_reply_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
+)
