3717570f6f94f0eaa386d743d4701cbb8976cbbe
Alec Choy <alecchoy@gmail.com>
2022-05-27 16:30:02 -0700

    finished create

--- W6D5/cats_99/app/controllers/cats_controller.rb
+++ W6D5/cats_99/app/controllers/cats_controller.rb
@@ -7,7 +7,26 @@ class CatsController < ApplicationController
 
     def show
         @cat = Cat.find_by(id: params[:id])
-        render json: @cat
+        render :show 
+    end
+
+    def create 
+        @cat = Cat.new(cat_params)
+        if @cat.save  
+            redirect_to cat_url(@cat)
+        else
+            render json: @cat.errors.full_messages, status: 422
+        end 
+
+    end
+
+    def
+
+
+    private
+
+    def cat_params
+        params.require(:cat).permit(:name, :birthday, :color, :sex, :description)
     end
 
 end
